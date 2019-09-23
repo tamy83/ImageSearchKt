852426eecf1ef4e7dd09f8df3554f7f7ae273893
Yenson Tam <yensontam@gmail.com>
2019-09-23 11:18:45 -0400

    add notes

--- app/src/main/java/com/example/imagesearchkt/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchkt/MainActivity.kt
@@ -24,6 +24,12 @@ class MainActivity : AppCompatActivity() {
     // lazy or lateinit
     // https://www.bignerdranch.com/blog/kotlin-when-to-use-lazy-or-lateinit/
     var imagesRecyclerView : RecyclerView? = null
+
+    // not null type. Will throw exception if tried to access before it is set on line 37
+    //lateinit var imagesRecyclerView : RecyclerView
+
+    // non null type, initializes by the supplied lambda unless it has been already assigned
+    //val imagesRecyclerView by lazy { findViewById<RecyclerView>(R.id.imagesRecyclerView) }
     var imageAdapter : ImageAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,12 +78,12 @@ class MainActivity : AppCompatActivity() {
         imageSearchTask.execute(searchTerms, countString)
     }
 
-    class ImageSearchTask : AsyncTask<String, Void, List<Image>>() {
+    internal inner class ImageSearchTask : AsyncTask<String, Void, List<Image>>() {
 
         var subscriptionKey = "c836d36105aa4386878b3f87c71b74c1"
         var host = "https://api.cognitive.microsoft.com"
         var path = "/bing/v7.0/images/search"
-        var imageAdapter: ImageAdapter ?= null
+        //var imageAdapter: ImageAdapter ?= null
 
         fun setAdapter(adapter: ImageAdapter?) {
             imageAdapter = adapter
@@ -90,6 +96,7 @@ class MainActivity : AppCompatActivity() {
                             + "?q=" + URLEncoder.encode(params[0], "UTF-8")
                             + "&count=" + URLEncoder.encode(params[1], "UTF-8")
                 )
+                // casting
                 val connection = url.openConnection() as HttpsURLConnection
                 connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey)
 
@@ -99,11 +106,14 @@ class MainActivity : AppCompatActivity() {
                 stream.close()
 
                 val parser = JsonParser()
-                val json = parser.parse(response).asJsonObject
+                // convenience method, should check isJsonObject before calling
+                //val json = parser.parse(response).asJsonObject
+                val json = parser.parse(response) as JsonObject
 
                 val images = ArrayList<Image>()
                 val results = json.getAsJsonArray("value")
-                for (i in 0 until results.size()) {
+                //for (i in 0 until results.size()) {
+                for (i in 0..results.size()) {
                     val imageJson = results.get(i) as JsonObject
                     val image = BingImage(imageJson)
                     images.add(image)
